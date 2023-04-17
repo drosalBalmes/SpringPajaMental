21dd140a47825bbe4c2c56423361c869d0af65c5
david <drosal@jaumebalmes.net>
2023-04-17 19:37:54 +0200

    Try2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,14 @@
 package org.example;
+import org.example.com.balmes.configuration.HolaMonConfiguracio;
+import org.example.com.balmes.controller.HolaMonController;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
     public static void main(String[] args) {
-        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        ApplicationContext context = new AnnotationConfigApplicationContext(HolaMonConfiguracio.class);
+        HolaMonController holaMonController = context.getBean(HolaMonController.class);
 
     }
 }
